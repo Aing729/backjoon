d4aecb7d0274c4b7cda5f87f5e53209694ffa57d
Aing729 <ddulgi158@gmail.com>
2025-04-21 08:19:58 +0900

    Add files via upload

---
+++ dptl.py
@@ -0,0 +1,165 @@
+# n= int(input())
+# for i in range(0,10) :
+#     print('k5 있')
+# 중고차들 = ['k5', 'BMW', "Tico"]
+# for i in 중고차들 :
+#     print(i * 3)
+
+# def d(a) :
+#     for i in range(0,10):
+#         print(a+i)
+# d(11092)
+# def f():
+#     return 10
+# print(f())
+# import sys
+# n,m = map(int, sys.stdin.readline().split())
+# def fac(n):
+#     sum = 1
+#     for i in range(1, n+1):
+#         sum *= i
+#     return sum
+# a=fac(m)//(fac(n)*fac(m-n))
+# print(a)
+#mCn => m!/n!(m-n)!
+# n=int(input())
+# for i in range(0,n):
+#     print(" "*i+'*'*(n-i),end="\n")
+# import sys
+# a,b = map(int, sys.stdin.readline().split())
+# if a>b:
+#     print(">")
+# elif a<b:
+#     print("<")
+# else:
+#     print("==")
+# import sys
+# n,m = map(int, sys.stdin.readline().split())
+# if m>=n:
+#     print(m-n)
+# else:
+#     print(n-m)
+# a, b = map(int, input().split())
+# if a > 0 and b > 0:
+#     print(1)
+# elif a < 0 and b > 0:
+#     print(2)
+# elif a < 0 and b < 0:
+#     print(3)
+# elif a > 0 and b < 0:
+#     print(4)
+# n,m = map(int,input().split())
+# if 1<=m<=n<=10**1000:
+#     print(n//m)
+#     print(n%m)
+# print("  ___  ___  ___")
+# print("  | |__| |__| |")
+# print("  |           |")
+# print("   \_________/")
+# print("    \_______/")
+# print("     |     |")
+# print("     |     |")
+# print("     |     |")
+# print("     |     |")
+# print("     |_____|")
+# print("  __/       \__")
+# print(" /             \ ")
+# print("/_______________\ ")
+# n= int(input())
+# for i in range(1,n+1):
+#     print("*"*i)
+# n=int(input())
+# for i in range(1,n+1):
+#     print(" "*(n-i)+"*"*i)
+# n=int(input())
+# for i in range(0,n):
+#     print("*"*(n-i))
+# n=int(input())
+# for i in range(1,10):
+#     print(f"{n} * {i} = {n*i}")
+# a = list(input())
+# for i in range(len(a)):
+#     k = a[i]
+#     if k.isupper():
+#         print(k.lower(), end='')
+#     else:
+#         print(k.upper(), end='')
+# a,b,c,d,e,f = map(int,input().split())
+# print(f"{1-a} {1-b} {2-c} {2-d} {2-e} {8-f}")
+# print("       _.-;;-._")
+# print("'-..-'|   ||   |")
+# print("'-..-'|_.-;;-._|")
+# print("'-..-'|   ||   |")
+# print("'-..-'|_.-''-._|")
+# print(".  .   .")
+# print("|  | _ | _. _ ._ _  _")
+# print("|/\|(/.|(_.(_)[ | )(/.")
+# print("     /~\ ")
+# print("    ( oo|")
+# print("    _\=/_")
+# print("   /  _  \ ")
+# print("  //|/.\|\\" +"\ ")
+# print(" ||  \ /  ||")
+# print("============")
+# print("|          |")
+# print("|          |")
+# print("|          |")
+# a,b=map(int,input().split())
+# while not (a ==0 and b ==0):
+#     print(a+b)
+#     a,b=map(int,input().split())
+# def sigma(b):
+#     sum = 0
+#     for i in range(1, b+1):
+#         sum += i
+#     return sum
+# n=int(input())
+# t = sigma(n)
+# print(t)
+# print("    8888888888  888    88888")
+# print("   88     88   88 88   88  88")
+# print("    8888  88  88   88  88888")
+# print("       88 88 888888888 88   88")
+# print("88888888  88 88     88 88    888888")
+# print("")
+# print("88  88  88   888    88888    888888")
+# print("88  88  88  88 88   88  88  88")
+# print("88 8888 88 88   88  88888    8888")
+# print(" 888  888 888888888 88  88      88")
+# print("  88  88  88     88 88   88888888")
+# import sys
+# t=int(input())
+# A,B=map(int,sys.stdin.readline().split())
+# for i in range(1,t+1):
+#     print(f"Case #{i}: {A+B}")
+#     A,B=map(int,sys.stdin.readline().split())
+
+# a,b,c = map(int,input().split())
+# print(a+b-c)
+# print(int(str(a)+str(b))-int(str(c)))
+# t=int(input())
+# for i in range(t):
+#     a,b=map(int,input().split())
+#     print(a+b)
+# while 1:
+#     try:
+#         a,b=map(int,input().split())
+#         print(a+b)
+#     except:
+#         break
+# while 1:
+#     a,b=map(int,input().split())
+#     if a==0 and b==0:
+#         break
+#     print(a+b)
+# s=list(input())
+# i=int(input())
+# if i-1<=len(s):
+#     print(s[i-1])
+# print("         ,r'"+'"7')
+# print("r`-_   ,'  ,/")
+# print(' \. "'+". L_r'")
+# print("   `~\/ ")
+# print("      |")
+# print("      |")
+a=input()
